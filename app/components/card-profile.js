00c94ec644a1abbdbf19b7277c33e709fede1cf1
C. Yarbrough <cryarbrough@gmail.com>
2017-09-20 12:30:13 -0600

    Added card-profile

---
+++ app/components/card-profile.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+const { Component, computed } = Ember;
+
+export default Component.extend({
+  /******************************** Passed-In Variables */
+  /**
+   * Job Profile model 
+   * @var {*}
+   */
+  jobProfile: null,
+  /**
+   * Prospect Profile model 
+   * @var {*}
+   */
+  prospectProfile: null,
+
+  /******************************** Variables */
+  /**
+   * Components class names
+   * @var []
+   */
+  classNames: ['card-profile'],
+  /**
+   * Alias of jobEssentials
+   * @var {*}
+   */
+  job: computed.alias('jobProfile'),
+  /**
+   * Alias of prospectEssentials
+   * @var {*}
+   */
+  prospect: computed.alias('prospectProfile')
+});
