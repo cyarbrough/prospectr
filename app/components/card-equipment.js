b99ca99897a8aea7e2f5fd2524542fa68f814bdd
C. Yarbrough <cryarbrough@gmail.com>
2017-09-20 12:34:38 -0600

    Added card-equipment

---
+++ app/components/card-equipment.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+const { Component, computed } = Ember;
+
+export default Component.extend({
+  /******************************** Passed-In Variables */
+  /**
+   * Job Equipment model 
+   * @var {*}
+   */
+  jobEquipment: null,
+  /**
+   * Prospect Equipment model 
+   * @var {*}
+   */
+  prospectEquipment: null,
+
+  /******************************** Variables */
+  /**
+   * Components class names
+   * @var []
+   */
+  classNames: ['card-equipment'],
+  /**
+   * Alias of jobEssentials
+   * @var {*}
+   */
+  job: computed.alias('jobEquipment'),
+  /**
+   * Alias of prospectEssentials
+   * @var {*}
+   */
+  prospect: computed.alias('prospectEquipment')
+});
---
+++ tests/integration/components/card-equipment-test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describe('Integration | Component | card equipment', function() {
+  setupComponentTest('card-equipment', {
+    integration: true
+  });
+
+  it('renders', function() {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+    // Template block usage:
+    // this.render(hbs`
+    //   {{#card-equipment}}
+    //     template content
+    //   {{/card-equipment}}
+    // `);
+
+    this.render(hbs`{{card-equipment}}`);
+    expect(this.$()).to.have.length(1);
+  });
+});
