7c8e66712776e272b36c2f4045499c789375e73e
C. Yarbrough <cryarbrough@gmail.com>
2017-09-20 20:08:43 -0600

    Flair

--- app/components/card-info.js
+++ app/components/card-info.js
@@ -24,7 +24,7 @@ export default Component.extend({
    */
   cardIcon: computed('isEmployer', function () {
     if (this.get('isEmployer')) {
-      return 'building';
+      return 'building-o';
     }
     return 'child';
   }),
